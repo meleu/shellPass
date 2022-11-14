1881e3effed2d032120ea4a3c832b111673f80f7
meleu <meleu.dev@gmail.com>
2022-11-14 12:47:47 -0300

    fix indentation

--- shellPass.sh
+++ shellPass.sh
@@ -22,84 +22,86 @@ ITALIC=$(tput dim)
 MAX=$1
 #----------FUNC-------------------------------------------------------------|
 clear
-_getsize(){
-while [[ -z "$MAX" || $MAX == *[^[:digit:]]* || $MAX == 0 ]];  do
-  clear
-  echo -e "$USAGE \n"
-  echo -e "${BOLD} Enter the QUANTITY of characters for the password or [Q] to quit: ${FECHA}"
-  read -r MAX
-  [[ $MAX == [qQ] ]] && exit 0
-done
-return 0
+_getsize() {
+  while [[ -z "$MAX" || $MAX == *[^[:digit:]]* || $MAX == 0 ]]; do
+    clear
+    echo -e "$USAGE \n"
+    echo -e "${BOLD} Enter the QUANTITY of characters for the password or [Q] to quit: ${FECHA}"
+    read -r MAX
+    [[ $MAX == [qQ] ]] && exit 0
+  done
+  return 0
 }
-_makepass(){
-case $MAX in
-  q|Q)
-    echo -e "Quiting..."
-    exit 0
-    ;;
-  ''|*[!0-9]*)
-    echo -e "${BLINK} Invalid option. ${FECHA}"
-    _getsize
-    ;;
-  [0-9]*)
-    echo -e "${BOLD} Enter the TYPE of password complexity you want: ${FECHA}
+_makepass() {
+  case $MAX in
+    q | Q)
+      echo -e "Quiting..."
+      exit 0
+      ;;
+    '' | *[!0-9]*)
+      echo -e "${BLINK} Invalid option. ${FECHA}"
+      _getsize
+      ;;
+    [0-9]*)
+      echo -e "${BOLD} Enter the TYPE of password complexity you want: ${FECHA}
     ${ITALIC} 1 - Password only numbers ${FECHA}
     ${ITALIC} 2 - Password with LeTtErS and numb3rs ${FECHA}
-    ${ITALIC} 3 - Password with LeTtErS, numb3rs and Speci@l Ch@r@ct&rs ${FECHA}";
-    read -rsn 1 TIPO;
+    ${ITALIC} 3 - Password with LeTtErS, numb3rs and Speci@l Ch@r@ct&rs ${FECHA}"
+      read -rsn 1 TIPO
 
-    case "$TIPO" in
-      q|Q)
-        echo -e "Bye.." && exit 0
-        ;;
-      ''|*[!0-9]*)
-        echo -e "${BLINK} Enter only numbers referring to the TYPE of the password ${FECHA}"
-        return 2
-        ;;
-      1)
-        PASS=$(cat /dev/urandom LC_ALL=C | tr -dc '0-9' | head -c "$MAX")
-        command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-        echo -e "${BOLD}$PASS${FECHA}"
-        ;;
-      2)
-      PASS=$(cat /dev/urandom LC_ALL=C | tr -dc 'A-Za-z0-9' | head -c "$MAX")
-      command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-      echo -e "${BOLD}$PASS${FECHA}"
+      case "$TIPO" in
+        q | Q)
+          echo -e "Bye.." && exit 0
+          ;;
+        '' | *[!0-9]*)
+          echo -e "${BLINK} Enter only numbers referring to the TYPE of the password ${FECHA}"
+          return 2
+          ;;
+        1)
+          PASS=$(cat /dev/urandom LC_ALL=C | tr -dc '0-9' | head -c "$MAX")
+          command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
+          echo -e "${BOLD}$PASS${FECHA}"
+          ;;
+        2)
+          PASS=$(cat /dev/urandom LC_ALL=C | tr -dc 'A-Za-z0-9' | head -c "$MAX")
+          command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
+          echo -e "${BOLD}$PASS${FECHA}"
+          ;;
+        3)
+          PASS=$(cat /dev/urandom LC_ALL=C \
+            | tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$MAX")
+          command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
+          echo -e "${BOLD}$PASS${FECHA}"
+          ;;
+        *)
+          echo -e "${BLINK} Use only the options [1,2,3] ${FECHA}"
+          return 1
+          ;;
+      esac
       ;;
-      3)
-        PASS=$(cat /dev/urandom LC_ALL=C |
-          tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_{|}~' | head -c "$MAX")
-        command -v xclip > /dev/null && echo -n "$PASS" | xclip -sel copy || echo -n "$PASS" | pbcopy 2> /dev/null
-        echo -e "${BOLD}$PASS${FECHA}"
-        ;;
-      *)
-        echo -e "${BLINK} Use only the options [1,2,3] ${FECHA}"
-        return 1
-        ;;
-    esac
-esac
-_writeinfile
+  esac
+  _writeinfile
 }
 
-_writeinfile(){
-SCRIPT_PATH="${BASH_SOURCE:-$0}"
-ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
-ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
-echo "$(date '+%d/%m/%y %H:%M:%S') - $PASS" >> $ABS_DIRECTORY/history.log 
+_writeinfile() {
+  SCRIPT_PATH="${BASH_SOURCE:-$0}"
+  ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
+  ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
+  echo "$(date '+%d/%m/%y %H:%M:%S') - $PASS" >> $ABS_DIRECTORY/history.log
 }
 
-_askprint(){
-  read -rn 1 -p "[R]epeat, generate new password [Y/N] or [Q]uit program? [Y/N/R/Q]" OP; echo
+_askprint() {
+  read -rn 1 -p "[R]epeat, generate new password [Y/N] or [Q]uit program? [Y/N/R/Q]" OP
+  echo
 }
 #---------MAIN-------------------------------------------------------------------|
 
 case "$MAX" in
-  h | -h | --help )
+  h | -h | --help)
     echo -e "$USAGE"
     exit 0
     ;;
-  v | -v | --version )
+  v | -v | --version)
     echo -e "${BOLD} $VERSION ${FECHA}"
     exit 0
     ;;
@@ -108,9 +110,9 @@ case "$MAX" in
     while true; do
       if [[ "$OP" = [yYsS] ]]; then
         MAX=0
-      _getsize
-      _makepass
-      _askprint
+        _getsize
+        _makepass
+        _askprint
       elif [[ "$OP" = [rR] ]]; then
         _getsize
         _makepass
@@ -128,6 +130,6 @@ case "$MAX" in
     _makepass
     ;;
   *[!0-9]*)
-    echo -e "${BOLD} Invalid option, call again. ${FECHA}" && exit 1 
+    echo -e "${BOLD} Invalid option, call again. ${FECHA}" && exit 1
     ;;
 esac
